9344fe1f6d75852114036f7ea5b9e68a6a521e46
Alper <alperyorgun2@gmail.com>
2023-09-18 22:15:42 +0300

    fizzbuzz algorithm done

--- swift-fun-algorithms/FizzBuzz.playground/Contents.swift
+++ swift-fun-algorithms/FizzBuzz.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+
+/*
+ The general idea behind this algorithm is to run through a list of numbers and print Fizz when divisible by 3, Buzz when divisible by 5, and FizzBuzz when divisible by both 3 and 5.
+ */
+
+func calculateFizzBuzz(input : Int) -> String {
+    return input % 15 == 0 ? "\(input) FizzBuzz" : input % 3 == 0 ? "\(input) Fizz" : input % 5 == 0 ? "\(input) Buzz" : ""
+}
+
+// -- test func with 1000 mock data
+
+var oneThousandNumber : [Int] = [Int]()
+
+for number in 1...1000 {
+    oneThousandNumber.append(number)
+}
+
+for number in oneThousandNumber {
+    print(calculateFizzBuzz(input: number))
+}
+
