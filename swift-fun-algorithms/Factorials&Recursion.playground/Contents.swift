92336c80db1c48202f82fc47211885b9fcf36325
Alper <alperyorgun2@gmail.com>
2023-09-23 11:04:03 +0300

    calculate factorial with recursion done!

--- swift-fun-algorithms/Factorials&Recursion.playground/Contents.swift
+++ swift-fun-algorithms/Factorials&Recursion.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+
+
+func calculateFactorialsWRecursion(input : Int) -> Int {
+    if input == 0 {
+        return 1
+    }else{
+        return input * calculateFactorialsWRecursion(input: input - 1)
+    }
+}
+
+print(calculateFactorialsWRecursion(input: 0))
